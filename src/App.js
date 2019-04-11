17e921785d3b25f0d395ae54042e52b6a3f34269
David <dhahn123@gmail.com>
2019-04-10 22:24:24 -0700

    Install Material-UI Card Component, photo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Bookmarks from '@material-ui/icons/Bookmarks';
+import Navbar from './Navbar';
+import MediaCard from './Card'
+import TextField from '@material-ui/core/TextField';
 
 class App extends Component {
   render() {
     return (
-      <Button variant="contained" color="primary">
-      <Bookmarks></Bookmarks>
-        Welcome Material UI
-      </Button>
+      <div>
+        <Navbar />
+        <Button variant="contained" color="primary">
+          <Bookmarks></Bookmarks>
+          Welcome Material UI
+      </Button> <br />
+        <TextField
+          placeholder="Placeholder here"
+          label="Basic TextField" />
+        <MediaCard />
+      </div>
+
     );
   }
 }
